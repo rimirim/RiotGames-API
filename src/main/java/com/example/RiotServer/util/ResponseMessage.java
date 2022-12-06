1cd760606beafa5c63d1ca93959919b44d1eadcc
ARYoo <proposesj@naver.com>
2022-12-06 18:20:23 +0900

    Status Code 관련 유틸 추가

---
+++ src/main/java/com/example/RiotServer/util/ResponseMessage.java
@@ -0,0 +1,16 @@
+package com.example.RiotServer.util;
+
+public class ResponseMessage {
+
+    // COMMON MESSAGE
+    public static final String OK = "OK";
+    public static final String BAD_REQUEST = "BAD REQUEST";
+    public static final String DATA_REQUEST_SUCCESS = "DATA REQUEST SUCCESS";
+
+    // CHAMPION
+    public static final String CHAMPION_DATAS_INSERT_FAILED = "CHAMPION DATA INSERT FAILED";
+    public static final String CHAMPION_DATAS_ARE_NULL = "CHAMPION DATA INSERT FAILED";
+
+    public static final String RIOT_GAMES_API_KEY_EXPIRED = "RIOT GAMES API KEY EXPIRED";
+
+}
---
+++ src/main/java/com/example/RiotServer/util/StatusCode.java
@@ -0,0 +1,17 @@
+package com.example.RiotServer.util;
+
+public class StatusCode {
+
+    public static final int OK = 200;
+    public static final int ACCEPTED = 202;
+    public static final int NO_CONTENT = 204;
+
+    public static final int BAD_REQUEST = 400;
+    public static final int UNAUTHORIZED = 401;
+    public static final int FORBIDDEN = 403;
+    public static final int NOT_FOUND = 404;
+    public static final int FAILED = 420;
+
+    public static final int INTERNAL_ERROR = 500;
+
+}
