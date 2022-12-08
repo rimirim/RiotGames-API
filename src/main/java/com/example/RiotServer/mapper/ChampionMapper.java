10abf270198c6a0b1b303b6b17ec493ca4e13bd6
ARYoo <proposesj@naver.com>
2022-12-08 18:13:17 +0900

    Mapper 위치 수정

--- src/main/java/com/example/RiotServer/mapper/ChampionMapper.java
+++ src/main/java/com/example/RiotServer/mapper/ChampionMapper.java
@@ -2,10 +2,12 @@ package com.example.RiotServer.mapper;
 
 import com.example.RiotServer.entity.Champion;
 import org.apache.ibatis.annotations.Mapper;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 @Mapper
+@Repository
 public interface ChampionMapper {
     public int insertChampions (List<Champion> insert);
     public List<Champion> getChampionList();
